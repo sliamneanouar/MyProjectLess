073ca7d3128c7e722f6b3bb8e182af4f45a55fd2
sliamneanouar <slimaneanoir@gmail.com>
2018-04-01 22:38:17 +0200

    new format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule, Injectable, Component, OnInit, ModuleWithProviders} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import {Router, Routes, RouterModule} from '@angular/router';
+
+const appRoutes: Routes = [
+    {path: 'home', component: HomeComponent}
+];
+const appRoutingProviders: any[] = [];
+const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes, { useHash: true });
 
 @NgModule({
   declarations: [
@@ -14,7 +21,8 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes, { useHash: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
